a702561d148bde18a117d168e865c78b6665ed06
BillaSpace <SmokenX1@gmail.com>
2025-07-02 13:29:18 +0530

    Update mainhelper.py

--- dxbotz/utils/mainhelper.py
+++ dxbotz/utils/mainhelper.py
@@ -1,17 +1,13 @@
-# Copyright (C) 2024 DX-MODS
-#Licensed under the  AGPL-3.0 License;
-#you may not use this file except in compliance with the License.
-#Author ZIYAN
-#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
-
 import os
 from random import randint 
-#from yt.yt_dlp import YoutubeDL
 from yt_dlp import YoutubeDL
-from config import LOGGER,LOG_GROUP,BUG
+from config import LOGGER, LOG_GROUP, BUG
 from requests import get
 from asyncio import sleep 
 from asgiref.sync import sync_to_async
+
+COOKIES_PATH = os.path.join("cookies", "cookies.txt")
+
 @sync_to_async
 def parse_deezer_url(url):
     url = get(url).url
@@ -26,19 +22,18 @@ def parse_spotify_url(url):
         return url.split(":")[1]
     url = get(url).url
     parsed_url = url.replace("https://open.spotify.com/", "").split("/")
-    return parsed_url[0],parsed_url[1].split("?")[0]
+    return parsed_url[0], parsed_url[1].split("?")[0]
 
 @sync_to_async
-def thumb_down(link,deezer_id):
-    with open(f"/tmp/thumbnails/{deezer_id}.jpg","wb") as file:
+def thumb_down(link, deezer_id):
+    thumb_path = f"/tmp/thumbnails/{deezer_id}.jpg"
+    os.makedirs(os.path.dirname(thumb_path), exist_ok=True)
+    with open(thumb_path, "wb") as file:
         file.write(get(link).content)
-    return f"/tmp/thumbnails/{deezer_id}.jpg"
+    return thumb_path
 
 @sync_to_async
 def fetch_tracks(dz, item_type, item_id):
-    """
-    Fetches tracks from the provided URL.
-    """
     songs_list = []
     offset = 0
     if item_type == 'playlist':
@@ -51,10 +46,17 @@ def fetch_tracks(dz, item_type, item_id):
             cover = item.album.cover_xl
             thumb = item.album.cover_small
             deezer_id = item.id
-            songs_list.append({"name": track_name, "artist": track_artist, "album": track_album,"playlist_num": offset + 1,
-                                "cover": cover,"deezer_id": deezer_id,"thumb":thumb,"duration":item.duration})
+            songs_list.append({
+                "name": track_name,
+                "artist": track_artist,
+                "album": track_album,
+                "playlist_num": offset + 1,
+                "cover": cover,
+                "deezer_id": deezer_id,
+                "thumb": thumb,
+                "duration": item.duration
+            })
             offset += 1
-
             if len(items) == offset:
                 break
     elif item_type == 'album':
@@ -67,24 +69,35 @@ def fetch_tracks(dz, item_type, item_id):
             track_name = item.title
             track_artist = item.artist.name
             deezer_id = item.id
-            songs_list.append({"name": track_name, "artist": track_artist, "album": track_album,"playlist_num": offset + 1,
-                                "cover": cover,"deezer_id": deezer_id,"thumb": thumb,"duration": item.duration})
+            songs_list.append({
+                "name": track_name,
+                "artist": track_artist,
+                "album": track_album,
+                "playlist_num": offset + 1,
+                "cover": cover,
+                "deezer_id": deezer_id,
+                "thumb": thumb,
+                "duration": item.duration
+            })
             offset += 1
-
             if len(items) == offset:
                 break
     elif item_type == 'track':
         get_track = dz.get_track(item_id)
-        songs_list.append({"name": get_track.title, "artist": get_track.artist.name, "album": get_track.album.title,"playlist_num": offset + 1,
-                            "cover": get_track.album.cover_xl,"deezer_id": get_track.id,"thumb": get_track.album.cover_small,"duration": get_track.duration})
-
+        songs_list.append({
+            "name": get_track.title,
+            "artist": get_track.artist.name,
+            "album": get_track.album.title,
+            "playlist_num": offset + 1,
+            "cover": get_track.album.cover_xl,
+            "deezer_id": get_track.id,
+            "thumb": get_track.album.cover_small,
+            "duration": get_track.duration
+        })
     return songs_list
 
 @sync_to_async
-def fetch_spotify_track(client,item_id):
-    """
-    Fetch tracks from provided item.
-    """
+def fetch_spotify_track(client, item_id):
     item = client.track(track_id=item_id)
     track_name = item.get("name")
     album_info = item.get("album")
@@ -99,106 +112,102 @@ def fetch_spotify_track(client,item_id):
     genre = client.artist(artist_id=item['artists'][0]['uri'])['genres'][0] if len(client.artist(artist_id=item['artists'][0]['uri'])['genres']) > 0 else ""
     offset = 0
     return {
-            "name": track_name,
-            "artist": track_artist,
-            "album": track_album,
-            "year": track_year,
-            "num_tracks": album_total,
-            "num": track_num,
-            "playlist_num": offset + 1,
-            "cover": cover,
-            "genre": genre,
-            "deezer_id": deezer_id,
-        }
+        "name": track_name,
+        "artist": track_artist,
+        "album": track_album,
+        "year": track_year,
+        "num_tracks": album_total,
+        "num": track_num,
+        "playlist_num": offset + 1,
+        "cover": cover,
+        "genre": genre,
+        "deezer_id": deezer_id,
+    }
 
 @sync_to_async
 def download_songs(item, download_directory='.'):
     file = f"{download_directory}/{item['name']} - {item['artists'][0]['name']}"
-    query = f"{item['name']} - {item['artists'][0]['name']}".replace(":", "").replace("\"", "")
+    query = f"{item['name']} {item['artists'][0]['name']}".replace(":", "").replace("\"", "")
     ydl_opts = {
-        'format': "bestaudio",
+        'format': "bestaudio/best",
         'default_search': 'ytsearch',
         'noplaylist': True,
         "nocheckcertificate": True,
-        "outtmpl": file,
+        "outtmpl": f"{file}.%(ext)s",
         "quiet": True,
         "addmetadata": True,
-        "prefer_ffmpeg": False,
+        "prefer_ffmpeg": True,
         "geo_bypass": True,
-
-        "nocheckcertificate": True,
-        "postprocessors": [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '320'}],
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "postprocessors": [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        "embedthumbnail": True,
+        "writethumbnail": False,
     }
-
+    if os.path.isfile(COOKIES_PATH):
+        ydl_opts["cookiefile"] = COOKIES_PATH
     with YoutubeDL(ydl_opts) as ydl:
         try:
-            video = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]['id']
-            info = ydl.extract_info(video)
+            info = ydl.extract_info(f"ytsearch:{query}", download=True)
             filename = ydl.prepare_filename(info)
-            return f"{filename}.mp3"
+            return f"{filename}"
         except IndexError:
-            pass
-            quer = f"{item['name']} lyrics"
-            video = ydl.extract_info(f"ytsearch:{quer}", download=False)['entries'][0]['id']
-            info = ydl.extract_info(video)
+            query = f"{item['name']} lyrics"
+            info = ydl.extract_info(f"ytsearch:{query}", download=True)
             filename = ydl.prepare_filename(info)
-            return f"{filename}.mp3" 
-        except (IOError,BrokenPipeError):
-            pass
-            video = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]['id']
-            info = ydl.extract_info(video)
-            filename = ydl.prepare_filename(info)
-            return f"{filename}.mp3"
+            return f"{filename}"
         except Exception as e:
-            LOGGER.error(e)
-
+            LOGGER.error(f"Download failed for {query}: {e}")
+            raise e
 
 @sync_to_async
 def download_dez(song, download_directory='.'):
     file = f"{download_directory}/{song['name']} - {song['artist']}"
-    query = f"{song.get('name')} - {song.get('artist')}".replace(":", "").replace("\"", "")
+    query = f"{song.get('name')} {song.get('artist')}".replace(":", "").replace("\"", "")
     ydl_opts = {
-        'format': "bestaudio",
+        'format': "bestaudio/best",
         'default_search': 'ytsearch',
         'noplaylist': True,
         "nocheckcertificate": True,
-        "outtmpl": file,
+        "outtmpl": f"{file}.%(ext)s",
         "quiet": True,
         "addmetadata": True,
-        "prefer_ffmpeg": False,
+        "prefer_ffmpeg": True,
         "geo_bypass": True,
-
-        "nocheckcertificate": True,
-        "postprocessors": [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'flac', 'preferredquality': '824'}],
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "postprocessors": [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        "embedthumbnail": True,
+        "writethumbnail": False,
     }
-
+    if os.path.isfile(COOKIES_PATH):
+        ydl_opts["cookiefile"] = COOKIES_PATH
     with YoutubeDL(ydl_opts) as ydl:
         try:
-            video = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]['id']
-            info = ydl.extract_info(video)
+            info = ydl.extract_info(f"ytsearch:{query}", download=True)
             filename = ydl.prepare_filename(info)
-            return f"{filename}.flac"
+            return f"{filename}"
         except IndexError:
-            pass
-            quer = f"{song['name']} lyrics"
-            video = ydl.extract_info(f"ytsearch:{quer}", download=False)['entries'][0]['id']
-            info = ydl.extract_info(video)
+            query = f"{song['name']} lyrics"
+            info = ydl.extract_info(f"ytsearch:{query}", download=True)
             filename = ydl.prepare_filename(info)
-            return f"{filename}.flac" 
-        except (IOError,BrokenPipeError):
-            pass
-            video = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]['id']
-            info = ydl.extract_info(video)
-            filename = ydl.prepare_filename(info)
-            return f"{filename}.flac"
+            return f"{filename}"
         except Exception as e:
-            LOGGER.error(e)
+            LOGGER.error(f"Download failed for {query}: {e}")
+            raise e
 
 @sync_to_async
-def copy(P,A):
+def copy(P, A):
     P.copy(LOG_GROUP)
     A.copy(LOG_GROUP)
+
 @sync_to_async
-def forward(A,P):
+def forward(A, P):
     A.copy(LOG_GROUP)
     P.copy(LOG_GROUP)
