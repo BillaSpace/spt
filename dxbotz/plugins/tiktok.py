06ebea63c317b263ab6c4646ac5b8b2b5a7a715f
BillaSpace <SmokenX1@gmail.com>
2025-07-02 11:11:20 +0530

    .

--- dxbotz/plugins/tiktok.py
+++ dxbotz/plugins/tiktok.py
@@ -25,14 +25,14 @@ async def link_handler(Dxbotz, message):
                      await message.reply_photo(get_api['cover'])
                  except:
                      pass 
-              dump_file = await message.reply_video(get_api['links'][0]['a'], caption="Thank you for using - @SpotifyDownlodbot")
+              dump_file = await message.reply_video(get_api['links'][0]['a'], caption="Thank you for using - @BillaDLbot")
            except KeyError:
                return await message.reply("Invalid TikTok video url. Please try again.")
            except Exception:
                snd_msg=await message.reply(get_api['links'][0]['a'])
                await asyncio.sleep(1)
                try:
-                  dump_file = await message.reply_video(get_api['links'][0]['a'],caption="Thank you for using - @SpotifyDownlodbot")
+                  dump_file = await message.reply_video(get_api['links'][0]['a'],caption="Thank you for using - @BillaDLbot")
                   await snd_msg.delete()
                except Exception:
                    pass
@@ -45,4 +45,4 @@ async def link_handler(Dxbotz, message):
             if DUMP_GROUP:
                await dump_file.copy(DUMP_GROUP)
             await m.delete()
-        await message.reply("Check out @dxmods_updates(updates)  @ziyan_dx(owner)")
+        await message.reply("Check out @BillaSpace(updates)  @x_ifeelram(owner)")
