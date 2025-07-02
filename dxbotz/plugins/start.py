5a89f6df256a22f685fb66e2d06f8e2e5c61c948
BillaSpace <SmokenX1@gmail.com>
2025-07-02 12:36:35 +0530

    .

--- dxbotz/plugins/start.py
+++ dxbotz/plugins/start.py
@@ -1,100 +1,129 @@
 # Copyright (C) 2024 DX-MODS
-#Licensed under the  AGPL-3.0 License;
-#you may not use this file except in compliance with the License.
-#Author ZIYAN
-#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+# Licensed under the AGPL-3.0 License;
+# Author: ZIYAN
+# Donate: https://www.buymeacoffee.com/ziyankp
 
 from asyncio import sleep
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-import random
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from dxbotz.utils.txt import dx
 from dxbotz.utils.database import db
 from config import START_PIC
+
+
+def to_smallcaps(text: str) -> str:
+    # Convert basic ASCII to small caps Unicode where possible
+    normal = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    small = "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀꜱᴛᴜᴠᴡxʏᴢ" + "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀꜱᴛᴜᴠᴡxʏᴢ"
+    return ''.join(small[normal.index(c)] if c in normal else c for c in text)
 
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client, message):
     user = message.from_user
     if not await db.is_user_exist(user.id):
-        await db.add_user(user.id)             
-    txt=f"👋 Hai {user.mention} \n𝙸'𝚖 𝙰 𝚊𝚍𝚟𝚊𝚗𝚌𝚎𝚍 𝚖𝚞𝚜𝚒𝚌 𝚍𝚘𝚠𝚗𝚕𝚘𝚊𝚍𝚎𝚛 𝚜𝚞𝚙𝚙𝚘𝚛𝚝𝚜 𝚂𝚙𝚘𝚝𝚒𝚏𝚢 𝚍𝚎𝚎𝚣𝚎𝚛 𝚢𝚘𝚞𝚝𝚞𝚋𝚎 𝚜𝚊𝚊𝚟𝚗!"
-    button=InlineKeyboardMarkup([[
-        InlineKeyboardButton("👼 𝙳𝙴𝚅𝚂 👼", callback_data='dev')
-        ],[
-        InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/BillaSpace'),
-        InlineKeyboardButton('🍂 𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url='https://t.me/BillaCore')
-        ],[
-        InlineKeyboardButton('🍃 𝙰𝙱𝙾𝚄𝚃', callback_data='about'),
-        InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿', callback_data='help')
-        ]])
+        await db.add_user(user.id)
+
+    txt = (
+        f"<b>👋 ᴡʜᴀᴛ'ꜱ ᴜᴘ {user.mention}</b>\n\n"
+        "<blockquote>"
+        + to_smallcaps(
+            "I'm a super advanced music downloader Pro-bot that supports multiple streaming platforms like "
+            "Spotify, YouTube, SoundCloud, Shazam, Deezer and social platforms like Facebook, Instagram Reels/IGTV, and TikTok!"
+        )
+        + "</blockquote>"
+    )
+
+    buttons = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🍃 𝙳𝙴𝚅𝚂", callback_data='dev')],
+        [
+            InlineKeyboardButton("📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂", url="https://t.me/BillaSpace"),
+            InlineKeyboardButton("🍂 𝚂𝚄𝙿𝙿𝙾𝚁𝚃", url="https://t.me/BillaCore")
+        ],
+        [
+            InlineKeyboardButton("🍃 𝙰𝙱𝙾𝚄𝚃", callback_data='about'),
+            InlineKeyboardButton("ℹ️ 𝙷𝙴𝙻𝙿", callback_data='help')
+        ]
+    ])
+
     if START_PIC:
-        await message.reply_photo(START_PIC, caption=txt, reply_markup=button)       
+        await message.reply_photo(START_PIC, caption=txt, reply_markup=buttons)
     else:
-        await message.reply_text(text=txt, reply_markup=button, disable_web_page_preview=True)  
+        await message.reply_text(txt, reply_markup=buttons, disable_web_page_preview=True)
+
 
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
-    data = query.data 
+    data = query.data
+
     if data == "start":
-        await query.message.edit_text(
-            text=f"""👋 Heyo {query.from_user.mention} \nI'm An Super Advanced Music Downloader That Supports Spotify Deezer Youtube JioSaavn Soundcloud & Social Platforms Like Insta FaceBook TikTok Etc! Join My Updates @BillaSpace For More SuperBots Like Me """,
-            reply_markup=InlineKeyboardMarkup( [[
-                InlineKeyboardButton("👼 𝙳𝙴𝚅𝚂", callback_data='dev')                
-                ],[
-                InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/BillaSpace'),
-                InlineKeyboardButton('🍂 𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url='https://t.me/BillaCore')
-                ],[
-                InlineKeyboardButton('🍃 𝙰𝙱𝙾𝚄𝚃', callback_data='about'),
-                InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿', callback_data='help')
-                ]]
-                )
+        txt = (
+            f"<b>👋 ʜᴇʏ {query.from_user.mention}!</b>\n\n"
+            "<blockquote>"
+            + to_smallcaps(
+                "I'm a super advanced music downloader that supports Spotify, Deezer, YouTube, JioSaavn, SoundCloud, "
+                "and even Instagram, TikTok, Facebook, and more!"
             )
+            + "</blockquote>\n"
+            + to_smallcaps("Stay tuned on @BillaSpace for more SuperBots like me!")
+        )
+
+        buttons = InlineKeyboardMarkup([
+            [InlineKeyboardButton("💌 𝙳𝙴𝚅𝚂", callback_data='dev')],
+            [
+                InlineKeyboardButton("📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂", url="https://t.me/BillaSpace"),
+                InlineKeyboardButton("🍂 𝚂𝚄𝙿𝙿𝙾𝚁𝚃", url="https://t.me/BillaCore")
+            ],
+            [
+                InlineKeyboardButton("🍃 𝙰𝙱𝙾𝚄𝚃", callback_data='about'),
+                InlineKeyboardButton("ℹ️ 𝙷𝙴𝙻𝙿", callback_data='help')
+            ]
+        ])
+        await query.message.edit_text(txt, reply_markup=buttons)
+
     elif data == "help":
         await query.message.edit_text(
             text=dx.HELP_TXT,
-            reply_markup=InlineKeyboardMarkup( [[
-               #⚠️ don't change source code & source link ⚠️ #
-               InlineKeyboardButton("❣️ 𝚂𝙾𝚄𝚁𝙲𝙴", url="https://github.com/Billa/spotidl")
-               ],[
-               InlineKeyboardButton("❤ ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url='https://t.me/chatspace_2')
-               ],[
-               InlineKeyboardButton("🔒 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
-               InlineKeyboardButton("◀️ 𝙱𝙰𝙲𝙺", callback_data = "start")
-               ]]
-            )
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("𝚂𝙾𝚄𝚁𝙲𝙴", url="https://github.com/Billa/spotidl")],
+                [InlineKeyboardButton("❤ ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url="https://t.me/chat_space2")],
+                [
+                    InlineKeyboardButton("🔒 𝙲𝙻𝙾𝚂𝙴", callback_data="close"),
+                    InlineKeyboardButton("◀️ 𝙱𝙰𝙲𝙺", callback_data="start")
+                ]
+            ])
         )
+
     elif data == "about":
         await query.message.edit_text(
-            text=dx.ABOUT_TXT,            
-            reply_markup=InlineKeyboardMarkup( [[
-               #⚠️ don't change source code & source link ⚠️ #
-               InlineKeyboardButton("❣️ 𝚂𝙾𝚄𝚁𝙲𝙴", url="https://github.com/Billa/spotidl")
-               ],[
-               InlineKeyboardButton("ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url="https://t.me/chatspace_2")
-               ],[
-               InlineKeyboardButton("🔒 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
-               InlineKeyboardButton("◀️ 𝙱𝙰𝙲𝙺", callback_data = "start")
-               ]]
-            )
+            text=dx.ABOUT_TXT,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("𝚂𝙾𝚄𝚁𝙲𝙴", url="https://github.com/Billa/spotidl")],
+                [InlineKeyboardButton("ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url="https://t.me/chat_space2")],
+                [
+                    InlineKeyboardButton("🔒 𝙲𝙻𝙾𝚂𝙴", callback_data="close"),
+                    InlineKeyboardButton("◀️ 𝙱𝙰𝙲𝙺", callback_data="start")
+                ]
+            ])
         )
+
     elif data == "dev":
         await query.message.edit_text(
             text=dx.DEV_TXT,
-            reply_markup=InlineKeyboardMarkup( [[
-               #⚠️ don't change source code & source link ⚠️ #
-               InlineKeyboardButton("❣️ 𝚂𝙾𝚄𝚁𝙲𝙴", url="https://github.com/DX-MODS/SpotifyDL")
-               ],[
-               InlineKeyboardButton("🖥️ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url="https://t.me/chatspace_2")
-               ],[
-               InlineKeyboardButton("🔒 𝙲𝙻𝙾𝚂𝙴", callback_data = "close"),
-               InlineKeyboardButton("◀️ 𝙱𝙰𝙲𝙺", callback_data = "start")
-               ]]
-            )
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("𝚂𝙾𝚄𝚁𝙲𝙴", url="https://github.com/Billa/spotidl")],
+                [InlineKeyboardButton("ᴄʜᴀᴛ ɢʀᴏᴜᴘ", url="https://t.me/chat_space2")],
+                [
+                    InlineKeyboardButton("🔒 𝙲𝙻𝙾𝚂𝙴", callback_data="close"),
+                    InlineKeyboardButton("◀️ 𝙱𝙰𝙲𝙺", callback_data="start")
+                ]
+            ])
         )
+
     elif data == "close":
         try:
             await query.message.delete()
-            await query.message.reply_to_message.delete()
+            if query.message.reply_to_message:
+                await query.message.reply_to_message.delete()
         except:
             await query.message.delete()
